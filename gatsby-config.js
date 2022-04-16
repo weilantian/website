0c133a0474c9b32ed0b363f821b23c07c26633c0
lantianwei <xiaolanlub@outlook.com>
2022-04-16 23:03:22 +0800

    fix: change mdx files to end with .md extension to make them detect by cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
         {
             resolve: 'gatsby-plugin-mdx',
             options: {
+                extensions: [`.mdx`, `.md`],
                 gatsbyRemarkPlugins: [`gatsby-remark-images`]
             }
         }
