a97bd8d05ab1d7e2f66c65e6d5ae32cb54671576
lantianwei <xiaolanlub@outlook.com>
2022-04-17 00:59:08 +0800

    feat: added alia for src/components

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
             resolve: 'gatsby-plugin-alias-imports',
             options: {
                 alias: {
-                    "@components": "src/components"
+                    "@components": "/src/components"
                 },
                 extensions: [
                     "js",
