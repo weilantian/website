2be59852147944d6cb50bda877bce63922027790
lantianwei <xiaolanlub@outlook.com>
2022-04-16 22:35:06 +0800

    refactor: better folder structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
             resolve: "gatsby-source-filesystem",
             options: {
                 name: "articles",
-                path: `${__dirname}/src/content/articles`,
+                path: `${__dirname}/content/articles`,
             },
         },
 
